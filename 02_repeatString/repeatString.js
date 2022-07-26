4e5eb41a1e9007fd81a9576fc37e0efba03390d8
hanwick <wickhannah02@gmail.com>
2022-07-26 10:48:01 -0500

    Complete exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(stringRepeat, numRepeat) {
+    if (numRepeat < 0) {
+        return "ERROR";
+    }
+    let newString = "";
+    for (let i = 0; i < numRepeat; i++) {
+        newString += stringRepeat;
+    }
+    return newString;
 };
 
 // Do not edit below this line
