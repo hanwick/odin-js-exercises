49600a5e49d2f7d646c55529e8ab7df9e4d1b894
hanwick <wickhannah02@gmail.com>
2022-07-26 11:13:15 -0500

    Complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+    const splitString = inputString.split("");
+    const reversedArray = [];
+    for (let i = inputString.length - 1; i >= 0; i--) {
+        reversedArray.push(splitString[i]);
+    }
+    let reversedStringCommas = reversedArray.toString();
+    let finalReversedString = reversedStringCommas.replaceAll(",", "");
+    return finalReversedString;
 };
 
 // Do not edit below this line
