30998668c7c9b62c24a94c18d4f101bbb4d202b0
hanwick <wickhannah02@gmail.com>
2022-07-26 13:33:48 -0500

    Complete exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(degreesF) {
+  let num1 = (degreesF - 32) * (5/9);
+  let resultC = num1.toFixed(1);
+  return Number(resultC);
 };
 
-const ctof = function() {
-
+const ctof = function(degreesC) {
+  let num1 = degreesC * (9/5) + 32;
+  let resultF = num1.toFixed(1);
+  return Number(resultF);
 };
 
 // Do not edit below this line
