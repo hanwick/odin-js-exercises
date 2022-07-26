5cc64812aada860769e75ab1854b100c6d4338be
hanwick <wickhannah02@gmail.com>
2022-07-26 13:00:06 -0500

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    const arrayEdit = args[0];
+    for (let i = 1; i < args.length; i++) {
+        for (let j = 0; j < arrayEdit.length; j++) {
+            if (arrayEdit[j] === args[i]) {
+                arrayEdit.splice(j,1);
+            }
+        }
+    }
+    return arrayEdit;
+}
 
-};
 
 // Do not edit below this line
 module.exports = removeFromArray;
