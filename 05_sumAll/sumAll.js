768ad2216881839bf6c360cc321bb822fcde414c
hanwick <wickhannah02@gmail.com>
2022-07-26 13:19:02 -0500

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(numOne, numTwo) {
+    let total = 0;
+    if (numOne < 0 || numTwo < 0) {
+        return "ERROR";
+    }
+    else if (typeof(numOne) != "number" || typeof(numTwo) != "number") {
+        return "ERROR";
+    }
+    else {
+        if (numOne < numTwo) {
+            for (let i = numOne; i <= numTwo; i++) {
+                total += i; 
+            }
+            return total;
+        }
+        for (let i = numTwo; i <= numOne; i++) {
+            total += i; 
+        }
+        return total;
+        }
 };
 
 // Do not edit below this line
